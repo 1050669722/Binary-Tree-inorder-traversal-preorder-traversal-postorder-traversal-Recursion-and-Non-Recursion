fff526707127963db3767a416e19eef92346eb73
1050669722 <42562697+1050669722@users.noreply.github.com>
2019-06-07 15:15:07 +0800

    Add files via upload

---
+++ 二叉树的中序遍历_非递归.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  7 15:04:56 2019
+
+@author: Administrator
+"""
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> list:        
+#        current = root
+#        stack = []
+#        ans = []
+#        while current or stack:
+#            if current:
+#                stack.append(current) #存入栈
+#                current = current.left #向左移
+#            else:
+#                current = stack.pop() #弹出栈
+#                ans.append(current.val)
+#                current = current.right #向右移
+#        return ans
+    
+        current = root
+        stack = []
+        ans = []
+        while current or stack:
+            if current:
+                stack.append(current)
+                current = current.left
+            else:
+                current = stack.pop()
+                ans.append(current.val)
+                current = current.right
+        return ans
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
