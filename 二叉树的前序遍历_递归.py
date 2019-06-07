62af453283b14c168ae8d40b59e8b8d900121058
1050669722 <42562697+1050669722@users.noreply.github.com>
2019-06-07 14:54:46 +0800

    Add files via upload

---
+++ 二叉树的前序遍历_递归.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  7 14:33:08 2019
+
+@author: Administrator
+"""
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> list:
+        if not root:
+            return []
+        ans = []
+        ans.append(root.val)
+        ans += self.preorderTraversal(root.left)
+        ans += self.preorderTraversal(root.right)
+        return ans
